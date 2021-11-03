3a78f9c1f0fecde651e2f40c23616a403d07f261
PNWMgithubBR <77895562+PNWMgithubBR@users.noreply.github.com>
2021-11-02 21:58:12 -0300

    tsxt

---
+++ mdc_ascii/app.js
@@ -0,0 +1,55 @@
+/*
+ * ASCII Camera
+ * http://idevelop.github.com/ascii-camera/
+ *
+ * Copyright 2013, Andrei Gheorghe (http://github.com/idevelop)
+ * Released under the MIT license
+ */
+
+(function() {
+	var asciiContainer = document.getElementById("ascii");
+	var capturing = false;
+
+	camera.init({
+		width: 160,
+		height: 120,
+		fps: 30,
+		mirror: true,
+
+		onFrame: function(canvas) {
+			ascii.fromCanvas(canvas, {
+				// contrast: 128,
+				callback: function(asciiString) {
+					asciiContainer.innerHTML = asciiString;
+				}
+			});
+		},
+
+		onSuccess: function() {
+			document.getElementById("info").style.display = "none";
+
+			const button = document.getElementById("button");
+			button.style.display = "block";
+			button.onclick = function() {
+				if (capturing) {
+					camera.pause();
+					button.innerText = 'resume';
+				} else {
+					camera.start();
+					button.innerText = 'pause';
+				}
+				capturing = !capturing;
+			};
+		},
+
+		onError: function(error) {
+			// TODO: log error
+		},
+
+		onNotSupported: function() {
+			document.getElementById("info").style.display = "none";
+			asciiContainer.style.display = "none";
+			document.getElementById("notSupported").style.display = "block";
+		}
+	});
+})();
