a6a0f1aa0eacc8b032b6c413a3f73234819034b0
PNWMgithubBR <77895562+PNWMgithubBR@users.noreply.github.com>
2021-12-10 19:04:08 -0300

    Create index.js

---
+++ GenerationBetaMemes/index.js
@@ -0,0 +1,6 @@
+<canvas id="canvas" width="1024" height="1024">
+  Canvas requires a browser that supports HTML5.
+</canvas>
+<img crossOrigin="Anonymous" id="the woks" src="https://i.kym-cdn.com/photos/images/newsfeed/002/229/102/5c5.jpg"/>
+  
+  
