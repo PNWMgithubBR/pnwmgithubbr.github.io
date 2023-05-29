d047b1398965358e8c29fd4204a9af9c2bb3babc
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-05-29 13:31:09 -0300

    limpeza de código e bug trocar  a cor

--- GenerationC_Memes/putz/cnvScript.js
+++ GenerationC_Memes/putz/cnvScript.js
@@ -69,99 +69,6 @@ function cores(cor) {
   }
 }
 
-background.onload = () => {
-  ctx.drawImage(background, 0, 0, WIDTH, HEIGHT);
-
-  // Adiciona sombra
-  ctx.shadowColor = corAtual; // Cor da sombra
-  ctx.shadowBlur = 7; // Desfoque da sombra
-  ctx.shadowOffsetX = 0; // Deslocamento horizontal da sombra
-  ctx.shadowOffsetY = 0; // Deslocamento vertical da sombra
-
-  ctx.font = "100px myFirstFont";
-  ctx.textAlign = "left";
-  ctx.textBaseline = "middle";
-  ctx.fillStyle = "white";
-  ctx.fillText("PUTZ", 50, HEIGHT / 2);
-
-  // Muda a cor da primeira letra para vermelho
-  ctx.fillStyle = corAtual;
-  ctx.fillText("P", 50, HEIGHT / 2);
-
-  // Limpa a configuração de cor
-  ctx.fillStyle = "white";
-};
-
-fotoInput.addEventListener("change", function (event) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onload = function (event) {
-    const img = new Image();
-    img.onload = function () {
-      ctx.drawImage(background, 0, 0, WIDTH, HEIGHT);
-
-      // Adiciona sombra
-      ctx.shadowColor = corAtual; // Cor da sombra
-      ctx.shadowBlur = 7; // Desfoque da sombra
-      ctx.shadowOffsetX = 0; // Deslocamento horizontal da sombra
-      ctx.shadowOffsetY = 0; // Deslocamento vertical da sombra
-
-      const desiredWidth = 300;
-      const aspectRatio = img.width / img.height;
-      const desiredHeight = desiredWidth / aspectRatio;
-      const posX = 400;
-      const posY = HEIGHT / 5;
-      ctx.drawImage(img, posX, posY, desiredWidth, desiredHeight);
-
-      ctx.font = "100px myFirstFont";
-      ctx.textAlign = "left";
-      ctx.textBaseline = "middle";
-      ctx.fillStyle = "white";
-      ctx.fillText("PUTZ", 50, HEIGHT / 2);
-
-      // Muda a cor da primeira letra para vermelho
-      ctx.fillStyle = corAtual;
-      ctx.fillText("P", 50, HEIGHT / 2);
-
-      // Limpa a configuração de cor
-      ctx.fillStyle = "white";
-
-      // Limpa as configurações de sombra
-      ctx.shadowColor = "transparent";
-      ctx.shadowBlur = 0;
-      ctx.shadowOffsetX = 0;
-      ctx.shadowOffsetY = 0;
-    };
-    img.src = event.target.result;
-  };
-  reader.readAsDataURL(file);
-});
-
-const direita = document.getElementById("direita");
-const esquerda = document.getElementById("esquerda");
-const cima = document.getElementById("cima");
-const baixo = document.getElementById("baixo");
-
-let posX = 400;
-let posY = HEIGHT / 5;
-
-direita.addEventListener("click", () => {
-  posX += 10;
-  desenhaImagem();
-});
-esquerda.addEventListener("click", () => {
-  posX -= 10;
-  desenhaImagem();
-});
-cima.addEventListener("click", () => {
-  posY -= 10;
-  desenhaImagem();
-});
-baixo.addEventListener("click", () => {
-  posY += 10;
-  desenhaImagem();
-});
-
 function desenhaImagem() {
   ctx.drawImage(background, 0, 0, WIDTH, HEIGHT);
 
@@ -200,3 +107,36 @@ function desenhaImagem() {
     reader.readAsDataURL(file);
   }
 }
+
+background.onload = () => {
+  desenhaImagem();
+};
+
+fotoInput.addEventListener("change", function (event) {
+  desenhaImagem();
+});
+
+const direita = document.getElementById("direita");
+const esquerda = document.getElementById("esquerda");
+const cima = document.getElementById("cima");
+const baixo = document.getElementById("baixo");
+
+let posX = 400;
+let posY = HEIGHT / 5;
+
+direita.addEventListener("click", () => {
+  posX += 10;
+  desenhaImagem();
+});
+esquerda.addEventListener("click", () => {
+  posX -= 10;
+  desenhaImagem();
+});
+cima.addEventListener("click", () => {
+  posY -= 10;
+  desenhaImagem();
+});
+baixo.addEventListener("click", () => {
+  posY += 10;
+  desenhaImagem();
+});
